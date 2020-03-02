ce8561252094d1feb3db987aa893935fe46f8ce8
Bram Soenens <bramsoenens@hotmail.be>
2020-03-02 09:36:25 +0100

    les 02/03/2020 start van de les

---
+++ pointers/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    cout << "Demo of pointers" << endl << endl;
+
+    int number = 42;
+    cout << "Address of nummber = " << &number << endl;
+
+    return 0;
+}
